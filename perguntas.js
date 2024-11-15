b8fb95948e8869b96326fd8534c4baf5d97bcb67
miks-vps <00001089366395sp@al.educacao.sp.gov.br>
2024-11-15 19:31:33 -0300

    Add files via upload
    projeto final

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Doramas',
+    'Qual o dorama coreano mais assistido em 2024?',
+    'Rainha das Lágrimas(Queen of Tears).'
+)
+
+criaCartao(
+    'Ciência',
+    'Qual o maior planeta do sistema solar?',
+    'Júpiter.'
+)
+
+criaCartao(
+    'Arte',
+    'Quem pintou a obra "A Noite Estrelada" ?',
+    'Vicent van Gogh'
+)
+
+criaCartao(
+    'O que é o que é:',
+    'O que é sempre igual, mas nunca é o mesmo?',
+    'O reflexo do espelho'
+)
